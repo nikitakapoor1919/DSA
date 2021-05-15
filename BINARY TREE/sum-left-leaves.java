e295bc9c1113d4e631a777c9181d1b36249a39d3
Nikita Kapoor <62166533+nikitakapoor1919@users.noreply.github.com>
2021-05-16 02:48:28 +0530

    Create sum-left-leaves.java

---
+++ BINARY TREE/sum-left-leaves.java
@@ -0,0 +1,21 @@
+class Solution {
+    // If Node is Left And It is leaf
+    int sum=0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        helper(root);
+        return sum;
+    }
+    private void helper(TreeNode root){
+        if(root==null){
+            return;
+        }
+        if(root.left!=null){
+            if(root.left.left==null && root.left.right==null){
+                sum+=root.left.val;
+            }
+        }
+        helper(root.left);
+        helper(root.right);
+
+    }
+}
