1cb79242332a8b41d6362c74ccd7b4131ba1d51b
Nikita Kapoor <62166533+nikitakapoor1919@users.noreply.github.com>
2021-05-15 13:50:51 +0530

    Update diameter-binary-tree.java

--- BINARY TREE/diameter-binary-tree.java
+++ BINARY TREE/diameter-binary-tree.java
@@ -21,3 +21,31 @@ class Solution {
         return Math.max(lh.ht+rh.ht+1,Math.max(lDia,rDia));
     }
 }
+
+********************************************************************
+class Solution {
+    class DiaPair{
+        int dia=0;
+        int ht=-1;
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        return helper(root).dia;
+    }
+    private DiaPair helper(TreeNode root){
+        if(root==null){
+            return new DiaPair();
+        }
+        DiaPair ldp=helper(root.left);
+        DiaPair rdp=helper(root.right);
+        
+        DiaPair sdp=new DiaPair();
+        int ld=ldp.dia;
+        int rd=rdp.dia;
+        int sd=ldp.ht+rdp.ht+2;
+        
+        sdp.dia=Math.max(sd,Math.max(ld,rd));
+        sdp.ht=Math.max(ldp.ht,rdp.ht)+1;
+        
+        return sdp;
+    }
+}
