3a2615d88c5fddbebcf0b403891cb31d330211fa
Nikita Kapoor <62166533+nikitakapoor1919@users.noreply.github.com>
2021-05-25 01:16:41 +0530

    Create intersection-ii.java

---
+++ ARRAYS/intersection-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums1.length;i++){
+            map.put(nums1[i],map.getOrDefault(nums1[i],0)+1);
+        }
+        ArrayList<Integer> al=new ArrayList<>();
+        for(int i=0;i<nums2.length;i++){
+            if(map.containsKey(nums2[i])){
+                if(map.get(nums2[i])>=1){
+                   al.add(nums2[i]);
+                   map.put(nums2[i],map.get(nums2[i])-1);
+                }
+            }
+        }
+        
+        int[] arr=new int[al.size()];
+        int k=0;
+        for(int i:al){
+            arr[k++]=i;
+        }
+        return arr;
+    }
+}
