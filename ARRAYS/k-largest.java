b5b787ac330388f8d88c22a1442bd5c3ad38aaa3
Nikita Kapoor <62166533+nikitakapoor1919@users.noreply.github.com>
2021-05-24 23:43:08 +0530

    Create k-largest.java

---
+++ ARRAYS/k-largest.java
@@ -0,0 +1,21 @@
+class Solution {
+    //Approch1: 0(NlogN)
+     public int findKthLargest2(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+    }
+    
+    //Approach2: O(kLogN)
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+    
+  
+}
