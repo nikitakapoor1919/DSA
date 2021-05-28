f0f8eb2639466fe1e5228c7b769775b6bc2bfd84
Nikita Kapoor <62166533+nikitakapoor1919@users.noreply.github.com>
2021-05-28 20:29:40 +0530

    Update longest-palindromic-subsequence.java

--- DYNAMIC PROGRAMMING/longest-palindromic-subsequence.java
+++ DYNAMIC PROGRAMMING/longest-palindromic-subsequence.java
@@ -1,80 +1,106 @@
-class Solution
-{
-    // A simple function to print leaf nodes of a binary tree
-    void printLeaves(Node node,ArrayList<Integer> list)
-    {
-        if (node == null)
-            return;
+package DynamicProgramming;
 
-        printLeaves(node.left,list);
-        // Print it if it is a leaf node
-        if (node.left == null && node.right == null){
-            list.add(node.data);
-        }
-        printLeaves(node.right,list);
-    }
+import java.util.Arrays;
 
-    // A function to print all left boundary nodes, except a leaf node.
-    // Print the nodes in TOP DOWN manner
-    void printBoundaryLeft(Node node,ArrayList<Integer> list)
-    {
-        if (node == null)
-            return;
-
-        if (node.left != null) {
-            // to ensure top down order, print the node
-            // before calling itself for left subtree
-            list.add(node.data);
-            printBoundaryLeft(node.left,list);
-        }
-        else if (node.right != null) {
-            list.add(node.data);
-            printBoundaryLeft(node.right,list);
-        }
-
-        // do nothing if it is a leaf node, this way we avoid
-        // duplicates in output
-    }
-
-    // A function to print all right boundary nodes, except a leaf node
-    // Print the nodes in BOTTOM UP manner
-    void printBoundaryRight(Node node,ArrayList<Integer> list)
-    {
-        if (node == null)
-            return;
-
-        if (node.right != null) {
-            // to ensure bottom up order, first call for right
-            // subtree, then print this node
-            printBoundaryRight(node.right,list);
-            list.add(node.data);
-        }
-        else if (node.left != null) {
-            printBoundaryRight(node.left,list);
-            list.add(node.data);
-        }
-        // do nothing if it is a leaf node, this way we avoid
-        // duplicates in output
-    }
-
-	ArrayList <Integer> printBoundary(Node node)
-	{
-	    ArrayList<Integer> list=new ArrayList<>();
-	    
-	   if (node == null)
-            return list;
-        list.add(node.data);
-
-        // Print the left boundary in top-down manner.
-        printBoundaryLeft(node.left,list);
-
-        // Print all leaf nodes
-        printLeaves(node.left,list);
-        printLeaves(node.right,list);
-
-        // Print the right boundary in bottom-up manner
-        printBoundaryRight(node.right,list);
-    
-	    return list;
+public class LongestPalindromicSubSequence {
+	
+	// TC=0(2^N) SC=Recursion Extra Space
+	public static int LPSR(String str, int si, int ei) {
+		// Negative Base Case
+		if (si > ei) {
+			return 0;
+		}
+		// Positive Base Case
+		if (si == ei) {
+			return 1;
+		}
+		char ch1 = str.charAt(si);
+		char ch2 = str.charAt(ei);
+		int ans;
+		if (ch1 == ch2) {
+			// 2 needs to be added as palindrome is would be of length 2
+			ans = LPSR(str, si + 1, ei - 1) + 2;
+		} else {
+			int o1 = LPSR(str, si, ei - 1);
+			int o2 = LPSR(str, si + 1, ei);
+			ans = Math.max(o1, o2);
+		}
+		return ans;
 	}
+
+	// TC=0(N*N) SC=0(N*N)+Recursion Extra Space
+	public static int LPSTD(String str, int si, int ei, int[][] strg) {
+		// Negative Base Case
+		if (si > ei) {
+			return 0;
+		}
+		// Positive Base Case
+		if (si == ei) {
+			return 1;
+		}
+		// Reuse
+		if (strg[si][ei] != -1) {
+			return strg[si][ei];
+		}
+		char ch1 = str.charAt(si);
+		char ch2 = str.charAt(ei);
+		int ans;
+		if (ch1 == ch2) {
+			// 2 needs to be added as palindrome is would be of length 2
+			ans = LPSR(str, si + 1, ei - 1) + 2;
+		} else {
+			int o1 = LPSR(str, si, ei - 1);
+			int o2 = LPSR(str, si + 1, ei);
+			ans = Math.max(o1, o2);
+		}
+		// Store
+		strg[si][ei] = ans;
+		return ans;
+	}
+
+	// TC=0(N*N) SC=0(N*N)
+	public static int LPSBU(String str) {
+		int n = str.length();
+		int[][] strg = new int[n][n];
+
+		for (int slide = 0; slide <= n - 1; slide++) {
+			for (int si = 0; si <= n - slide - 1; si++) {
+				int ei = si + slide;
+				// Negative Base Case
+				if (si > ei) {
+					strg[si][ei] = 0;
+				}
+				// Positive Base Case
+				if (si == ei) {
+					strg[si][ei] = 1;
+				} else {
+					char ch1 = str.charAt(si);
+					char ch2 = str.charAt(ei);
+					int ans;
+					if (ch1 == ch2) {
+						// 2 needs to be added as palindrome is would be of length 2
+						ans = LPSR(str, si + 1, ei - 1) + 2;
+					} else {
+						int o1 = LPSR(str, si, ei - 1);
+						int o2 = LPSR(str, si + 1, ei);
+						ans = Math.max(o1, o2);
+					}
+					strg[si][ei] = ans;
+				}
+			}
+		}
+		return strg[0][n - 1];
+	}
+
+	public static void main(String[] args) {
+		String s = "acdcga";
+		System.out.println(LPSR(s, 0, s.length() - 1));
+		int[][] strg = new int[s.length()][s.length()];
+		for (int i = 0; i < s.length(); i++) {
+			Arrays.fill(strg[i], -1);
+		}
+		System.out.println(LPSTD(s, 0, s.length() - 1, strg));
+		System.out.println(LPSBU(s));
+	}
+
 }
